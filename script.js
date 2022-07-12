3c1c65764e9e4219fbf3807413a351c6cbd5f1f3
Nnenna-udefi <miriamudefi@gmail.com>
2022-07-12 15:41:12 +0100

    selector

--- script.js
+++ script.js
@@ -1,124 +1,18 @@
 //we want to change the data of the 6 badges when we click on the different 
 //timeframes : daily, weekly and monthly
 // if i say let data = [
-//if (daily = 0) {}
-
-let data = [
-    {
-        "title": "Work",
-        "timeframes": {
-          "daily": {
-            "current": 5,
-            "previous": 7
-          },
-          "weekly": {
-            "current": 32,
-            "previous": 36
-          },
-          "monthly": {
-            "current": 103,
-            "previous": 128
-          }
-        }
-      },
-      {
-        "title": "Play",
-        "timeframes": {
-          "daily": {
-            "current": 1,
-            "previous": 2
-          },
-          "weekly": {
-            "current": 10,
-            "previous": 8
-          },
-          "monthly": {
-            "current": 23,
-            "previous": 29
-          }
-        }
-      },
-      {
-        "title": "Study",
-        "timeframes": {
-          "daily": {
-            "current": 0,
-            "previous": 1
-          },
-          "weekly": {
-            "current": 4,
-            "previous": 7
-          },
-          "monthly": {
-            "current": 13,
-            "previous": 19
-          }
-        }
-      },
-      {
-        "title": "Exercise",
-        "timeframes": {
-          "daily": {
-            "current": 1,
-            "previous": 1
-          },
-          "weekly": {
-            "current": 4,
-            "previous": 5
-          },
-          "monthly": {
-            "current": 11,
-            "previous": 18
-          }
-        }
-      },
-      {
-        "title": "Social",
-        "timeframes": {
-          "daily": {
-            "current": 1,
-            "previous": 3
-          },
-          "weekly": {
-            "current": 5,
-            "previous": 10
-          },
-          "monthly": {
-            "current": 21,
-            "previous": 23
-          }
-        }
-      },
-      {
-        "title": "Self Care",
-        "timeframes": {
-          "daily": {
-            "current": 0,
-            "previous": 1
-          },
-          "weekly": {
-            "current": 2,
-            "previous": 2
-          },
-          "monthly": {
-            "current": 7,
-            "previous": 11
-          }
-        }
-      }
-
-]
+//if (daily = 0) {
 
 
-const dailyBtn = document.querySelector('#daily-btn');
-const weeklyBtn = document.querySelector('#weekly-btn');
-const mothlyBtn = document.querySelector('#monthly-btn');
+const dailyBtn = document.querySelector('#daily');
+const weeklyBtn = document.querySelector('#weekly');
+const monthlyBtn = document.querySelector('#monthly');
 
-const hours = document.querySelectorAll('.hours');
-const lastTime = document.querySelectorAll('.last-time');
+const hours = document.querySelectorAll('.hrs');
+const lastTime = document.querySelectorAll('.previous');
 
 function dailyData(){
-    fetch('https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json')
+    fetch('https://raw.githubusercontent.com/Nnenna-udefi/time-tracking-dashboard/main/data.json')
         .then(response => response.json())
         .then(data => {
             hours.forEach((e,i) => {
@@ -129,7 +23,7 @@ function dailyData(){
 }
 
 function weeklyData(){
-    fetch('https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json')
+    fetch('https://raw.githubusercontent.com/Nnenna-udefi/time-tracking-dashboard/main/data.json')
         .then(response => response.json())
         .then(data => {
             hours.forEach((e,i) => {
@@ -138,9 +32,9 @@ function weeklyData(){
             })
         })
 }
-
-function mothlyData(){
-    fetch('https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json')
+// https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json
+function monthlyData(){
+    fetch('https://raw.githubusercontent.com/Nnenna-udefi/time-tracking-dashboard/main/data.json')
         .then(response => response.json())
         .then(data => {
             hours.forEach((e,i) => {
@@ -154,20 +48,20 @@ function mothlyData(){
 dailyBtn.addEventListener('click', () => {
     dailyBtn.classList.remove('activity-option')
     weeklyBtn.classList.add('activity-option')
-    mothlyBtn.classList.add('activity-option')
+    monthlyBtn.classList.add('activity-option')
     dailyData()
 })
 
 weeklyBtn.addEventListener('click', () => {
     weeklyBtn.classList.remove('activity-option')
-    mothlyBtn.classList.add('activity-option')
+    monthlyBtn.classList.add('activity-option')
     dailyBtn.classList.add('activity-option')
     weeklyData()
 })
 
-mothlyBtn.addEventListener('click', () => {
-    mothlyBtn.classList.remove('activity-option')
+monthlyBtn.addEventListener('click', () => {
+    monthlyBtn.classList.remove('activity-option')
     dailyBtn.classList.add('activity-option')
     weeklyBtn.classList.add('activity-option')
-    mothlyData()
+    monthlyData()
 })
